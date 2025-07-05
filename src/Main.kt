1f11ee361d1769c221d20dda239fa8c0d403585d
HIRedrain <redrain@yu.ac.kr>
2025-07-05 23:54:18 +0900

    :sparkles: feat - LV0. 옹알이 문제 푸는 중

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,17 @@
+
+import coding_test.Lv0
+
 fun main() {
     println("Hello World!")
+
+    // 선언
+    val lv0: Lv0 = Lv0()
+
+
+    // Lv0 - 옹알이
+    //
+    val babbling1: Array<String> = arrayOf("aya", "yee", "u", "maa", "wyeoo")
+    val babbling2: Array<String> = arrayOf("ayaye", "uuuma", "ye", "yemawoo", "ayaa", "Aya")
+    println("lv0.babbling(babbling1) : " + lv0.babbling(babbling1))
+    println("lv0.babbling(babbling2) : " + lv0.babbling(babbling1))
 }
