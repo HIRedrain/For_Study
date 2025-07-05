99ac6ea2e6c3d4f69f152baf1b60c054af8422cd
HIRedrain <redrain@yu.ac.kr>
2025-07-06 02:35:48 +0900

    :heavy_plus_sign: add - test 경로 및 다양한 처리

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,8 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+//	testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
+//	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.2")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
@@ -34,4 +36,26 @@ kotlin {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+tasks.test {
+	doFirst {
+//		println("📦 테스트 클래스 경로: ${classpath.asPath}")
+		println("🧩 테스트 대상 class 파일 목록 :")
+		fileTree("build/classes/kotlin/test").forEach {
+			if (it.name.endsWith(".class")) println(" - ${it}")
+		}
+	}
+
+	testLogging {
+		events("passed", "failed", "skipped", "standardOut", "standardError")
+		showStandardStreams = true
+		exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+	}
+}
+
+sourceSets {
+	test {
+		kotlin.srcDirs("src/test/kotlin")
+	}
 }
