e19ed6189eb46a6609d6c5d66fd7d48a14d074f9
HIRedrain <redrain@yu.ac.kr>
2025-07-06 00:41:50 +0900

    :tada: init - JUnit 사용을 위한 프로젝트 변경

---
+++ src/main/kotlin/for_study/ForStudyApplication.kt
@@ -0,0 +1,11 @@
+package for_study
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ForStudyApplication
+
+fun main(args: Array<String>) {
+	runApplication<ForStudyApplication>(*args)
+}
---
+++ src/test/kotlin/for_study/ForStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package for_study
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ForStudyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
