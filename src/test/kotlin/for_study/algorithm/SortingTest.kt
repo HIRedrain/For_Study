28dd5e75f40f8ea14dbcfa85f53d21f472c71012
HIRedrain <redrain@yu.ac.kr>
2025-07-07 19:53:59 +0900

    :white_check_mark: test - 선택 정렬 테스트 코드 작성

---
+++ src/test/kotlin/for_study/algorithm/SortingTest.kt
@@ -0,0 +1,40 @@
+/*
+* Algorithm - Sorting
+* Test 파일
+* 최초 작성 일자 : 2025.07.07
+*
+* ========================================================
+* 프로그램 수정 / 보완 이력
+* ========================================================
+* 날짜        수정 / 보완 내용
+* ========================================================
+* 2025.07.07  최초 작성 : 선택 정렬 테스트
+* ========================================================
+*/
+
+package for_study.algorithm
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class SortingTest {
+    val sorting: Sorting = Sorting()
+
+
+    @Test
+    @DisplayName("선택 정렬")
+    fun selectionSortTest() {
+        // given ; data 미리 준비
+        val intArray: IntArray = intArrayOf(2, 5, 7, 3, 1, 8, 6, 10, 4, 9)
+
+        // 예상 값 정답
+        val expectedIntArray: IntArray = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+        // when ; test 기능 수행
+        sorting.selectionSort(intArray)
+
+        // then ; 실제 결과 확인
+        assertArrayEquals(expectedIntArray, intArray)
+    }
+}
