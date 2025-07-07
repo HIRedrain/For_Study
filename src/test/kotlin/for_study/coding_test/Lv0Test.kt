87f2902e33edff125f780681301a85012094308a
HIRedrain <redrain@yu.ac.kr>
2025-07-07 21:53:27 +0900

    :white_check_mark: test - 나선형 행렬 출력 테스트 코드

--- src/test/kotlin/for_study/coding_test/Lv0Test.kt
+++ src/test/kotlin/for_study/coding_test/Lv0Test.kt
@@ -9,12 +9,14 @@
 * 날짜        수정 / 보완 내용
 * ========================================================
 * 2025.07.06  최초 작성 : 옹알이 테스트
+* 2025.07.07  나선형 행렬
 * ========================================================
 */
 
 package for_study.coding_test
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 //import kotlin.test.Test
@@ -24,6 +26,7 @@ class Lv0Test {
 
     @Test
     @DisplayName("조카가 발음 가능한 옹알이 단어 개수 확인")
+    @Disabled
     fun babllingsTest() {
         // given ; data 미리 준비
         val babbling1: Array<String> = arrayOf("aya", "yee", "u", "maa", "wyeoo")
@@ -40,6 +43,40 @@ class Lv0Test {
         // then ; 실제 결과 확인
         assertEquals(expected1, result1)
         assertEquals(expected2, result2)
+    }
 
+    @Test
+    @DisplayName("나선형 행렬")
+    fun spiralTest() {
+        // given ; data 미리 준비
+        val n5 = 5
+        val n2 = 2
+        val n0 = 0 // 안 되는 애
+        val n31 = 31 // 안 되는 애
+
+        // 예상 값 (정답)
+        val expectedN5: Array<IntArray> = arrayOf(
+            intArrayOf(1, 2, 3, 4, 5),
+            intArrayOf(16, 17, 18, 19, 6),
+            intArrayOf(15, 24, 25, 20, 7),
+            intArrayOf(14, 23, 22, 21, 8),
+            intArrayOf(13, 12, 11, 10, 9)
+        )
+        val expectedN2: Array<IntArray> = arrayOf(
+            intArrayOf(1, 2),
+            intArrayOf(4, 3)
+        )
+
+        // when ; test 기능 수행
+        val resultN5 = lv0.spiral(n5)
+        val resultN2 = lv0.spiral(n2)
+        val resultN0 = lv0.spiral(n0)
+        val resultN31 = lv0.spiral(n31)
+
+        // then ; 실제 결과 확인
+        assertArrayEquals(expectedN5, resultN5)
+        assertArrayEquals(expectedN2, resultN2)
+        assertArrayEquals(arrayOf(), resultN0)
+        assertArrayEquals(arrayOf(), resultN31)
     }
 }
